b3f6d4a00346411a2a3ac8520dcc295fef93479b
menyagah <menyagah27@gmail.com>
2021-08-20 23:13:09 +0300

    Install eslint and ficx linter errors

--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
-import { greeting } from './app';
+import './main.css';
 
-import "./main.css";
-
-
-const todoList =  [{
-    description:"wash the dishes",
-    completed: false,
-    id:1
+const todoList = [{
+  description: 'wash the dishes',
+  completed: false,
+  id: 1,
 },
 {
-    description:"complete the dishes",
-    completed: false,
-    id:2
+  description: 'complete the dishes',
+  completed: false,
+  id: 2,
 },
 {
-    description:"complete To Do list project",
-    completed: false,
-    id:3
-}
+  description: 'complete To Do list project',
+  completed: false,
+  id: 3,
+},
 ];
 
 const card = document.querySelector('.cardlist_main');
 card.innerHTML += `<li class="cardlist--sub main">Today's To Do</li>
-<li ><input type="text" class="cardlist--sub  input" placeholder="Add to your list..."></li>`
+<li ><input type="text" class="cardlist--sub  input" placeholder="Add to your list..."></li>`;
 
-todoList.forEach(({description}) => {
-    card.innerHTML += `
-    <li class="cardlist--sub"><input type="checkbox" name="" class="cardlist--check" id="">${description}</li>`
+todoList.forEach(({ description }) => {
+  card.innerHTML += `
+    <li class="cardlist--sub"><input type="checkbox" name="" class="cardlist--check" id="">${description}</li>`;
 });
 
-card.innerHTML += `<li class="cardlist--sub delete"><a href="#" class="delete_text">clear all completed</a></li>`
-
-
+card.innerHTML += '<li class="cardlist--sub delete"><a href="#" class="delete_text">clear all completed</a></li>';
