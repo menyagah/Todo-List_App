deb9bdf86caf0b6e76fdb2ce6c42237f2c91b155
menyagah <menyagah27@gmail.com>
2021-08-21 01:34:13 +0300

    Add server file to publish app on heroku

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const path = require('path');// eslint-disable-line import/no-extraneous-dependencies
-const express = require('express');// eslint-disable-line import/no-extraneous-dependencies
+const express = require('express');
+const path = require('path');
 
-const app = express();// eslint-disable-line import/no-extraneous-dependencies
+const port = process.env.PORT || 8080;
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));// eslint-disable-line import/no-extraneous-dependencies
-app.set('port', process.env.PORT || 8080);// eslint-disable-line import/no-extraneous-dependencies
+// the __dirname is the current directory from where the script is running
+app.use(express.static(path.resolve(__dirname, './dist')));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+});
+
+app.listen(port);
