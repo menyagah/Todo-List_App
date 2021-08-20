20b3b060e7922bfb439b9e91470400f9e1ffd459
menyagah <menyagah27@gmail.com>
2021-08-20 23:27:20 +0300

    Install eslint and fix linter errors

--- webpack.common.js
+++ webpack.common.js
@@ -1,37 +1,22 @@
-const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');// eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
-    entry: "./src/index.js",
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html",
-    }
-    )],
+  entry: './src/index.js',
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/template.html',
+  })],
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-                },
-            // {
-            //     test:/\.(svg|png|jpe?g|gif)$/i,
-            //     use: {
-            //         loader: "file-loader",
-            //         options: {
-            //             name: "[name].[hash].[ext]",
-            //             outputPath: "img/",
-            //         }
-            //     }
-            // }
-        ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
 
-}
-
-
+};
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,12 @@
-const path = require("path");
-const common = require("./webpack.common");
-const {merge} = require("webpack-merge");
-
+const path = require('path');
+const { merge } = require('webpack-merge');// eslint-disable-line import/no-extraneous-dependencies
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: "production",
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, "dist"),
-        publicPath: '/'
-    },
+  mode: 'production',
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
 });
-
-
