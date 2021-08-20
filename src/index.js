ff6783283a1dcb167aefe866954f97e53c20db65
menyagah <menyagah27@gmail.com>
2021-08-20 21:17:04 +0300

    Create elements dynamically on js, loop over them and render them

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import { greeting } from './app';
 
 import "./main.css";
-console.log(greeting);
+
+
+const todoList =  [{
+    description:"wash the dishes",
+    completed: false,
+    id:1
+},
+{
+    description:"complete the dishes",
+    completed: false,
+    id:2
+},
+{
+    description:"complete To Do list project",
+    completed: false,
+    id:3
+}
+];
+
+const card = document.querySelector('.cardlist_main');
+const liTop = document.createElement('li');
+liTop.innerHTML = `<li class="cardlist--sub main">Today's To Do</li>
+<li ><input type="text" class="cardlist--sub  input" placeholder="Add to your list..."></li>`
+card.appendChild(liTop)
+todoList.forEach(({description, completed, id}) => {
+    const li = document.createElement('li')
+    li.innerHTML = `
+    <li class="cardlist--sub"><input type="checkbox" name="" class="cardlist--check" id="">${description}</li>`
+    card.appendChild(li)
+});
