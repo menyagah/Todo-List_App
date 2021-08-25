fffc5d8803c48025f8321ff37547ff8ce1a21c14
menyagah <menyagah27@gmail.com>
2021-08-25 21:51:13 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import { checkbox } from './app.js'
 import './main.css';
 
 const todoList = [{
@@ -22,19 +21,11 @@ const card = document.querySelector('.cardlist_main');
 card.innerHTML += `<li class="cardlist--sub main">Today's To Do</li>
 <li ><input type="text" class="cardlist--sub  input" placeholder="Add to your list..."></li>`;
 
-
-todoList.forEach(({ description}) => {
+todoList.forEach(({ description }) => {
   const inputElement = document.createElement('input');
-  inputElement.setAttribute("type","name")
+  inputElement.setAttribute('type', 'name');
   card.innerHTML += `
     <li class="cardlist--sub"><input type="checkbox" name="" class="cardlist--check" id="check">${description}</li>`;
 });
 
 card.innerHTML += '<li class="cardlist--sub delete"><a href="#" class="delete_text">clear all completed</a></li>';
-
-const checkBox = document.getElementById('#check');
-checkBox.addEventListener('click', () => {
-  if (checkBox.checked) {
-      console.log('checkbox works');
-  }
-});
