b23bde23ea96e31dbca8ed81e6c2fdf5af7be3b7
menyagah <menyagah27@gmail.com>
2021-08-26 09:06:52 +0300

    Import module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import checkItem from './check.js';
 import './main.css';
 
 const todoList = [{
@@ -21,11 +22,18 @@ const card = document.querySelector('.cardlist_main');
 card.innerHTML += `<li class="cardlist--sub main">Today's To Do</li>
 <li ><input type="text" class="cardlist--sub  input" placeholder="Add to your list..."></li>`;
 
-todoList.forEach(({ description }) => {
+todoList.forEach(({ description, id }) => {
   const inputElement = document.createElement('input');
   inputElement.setAttribute('type', 'name');
   card.innerHTML += `
-    <li class="cardlist--sub"><input type="checkbox" name="" class="cardlist--check" id="check">${description}</li>`;
+    <li class="cardlist--sub"><input type="checkbox" name="" class="cardlist--check" id="check-${id}">${description}</li>`;
 });
 
 card.innerHTML += '<li class="cardlist--sub delete"><a href="#" class="delete_text">clear all completed</a></li>';
+
+for (let i = 0; i < todoList.length; i++){
+  const inCheck = document.querySelector(`#check-${todoList[i].id}`);
+  inCheck.addEventListener('input', (e)=> {
+    checkItem(i, e, todoList)
+  })
+};
