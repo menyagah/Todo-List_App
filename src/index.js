ea10f60a8f8c62f34777d29ca818af33ee49f974
menyagah <menyagah27@gmail.com>
2021-08-28 21:12:55 +0300

    Add delete function to all checked items

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import './main.css';
 import addItem from './addItem.js';
 import displayItems from './displayList.js';
-import attachInputEventListeners from './attachEventListeners';
+import attachInputEventListeners from './attachEventListeners.js';
+import clearAllBtn from './deleteItem.js';
 
 const card = document.querySelector('.cardlist_main');
 
 card.innerHTML = `<li class="cardlist--sub main">Today's To Do</li>
 <li ><input type="text" class="cardlist--sub  input" placeholder="Add to your list..."></li>`;
 
-card.innerHTML += `<li ><ul class="items"></ul></li>`;
+card.innerHTML += '<li ><ul class="items"></ul></li>';
 
 // Initial rendering of todo items after reading from locaStorage
 displayItems();
@@ -19,3 +20,4 @@ addItem();
 
 // Then attach "input" event listeners on the input checkboxes of todo items
 attachInputEventListeners();
+clearAllBtn();
